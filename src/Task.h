2baa403d6a3f7a40da09d57b1285e833f979f1d5
tobiasaluna <toobiasluna10@gmail.com>
2025-10-14 23:08:51 -0300

    clase task, falta definir metodos

--- src/Task.h
+++ src/Task.h
@@ -4,10 +4,17 @@
 
 class Task {
     private:
-
+        int id;
+        std::string title;
+        std::string description;
     public:
-
-
+        Task(int id, std::string title, std::string description);
+        int getId() const;
+        std::string getTitle() const;
+        std::string getDescription() const;
+        void setTitle(const std::string &newTitle);
+        void setDescription(const std::string &newDesc);
+        std::string toString() const;
 };
 
 
